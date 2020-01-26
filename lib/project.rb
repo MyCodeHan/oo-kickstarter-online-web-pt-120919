b757d6d2883871fa82a2310e881989e869f7bad1
Hannah Harrington <hannahharrington@outlook.com>
2020-01-26 15:31:35 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,13 @@
-class Backer
-  attr_accessor :name, :backed_projects
+class Project
+  attr_accessor :title, :backers
 
-  def initialize(name)
-    @name = name
-    @backed_projects = []
+  def initialize(title)
+    @title = title
+    @backers = []
   end
 
-  def back_project(project)
-    @backed_projects << project
-    project.backers << self
-
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
-
 end
